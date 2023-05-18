0646f0dd594bb5280c1177dfc0d769e20dde76a8
BuiThiThuyNgoc <19521904@gm.uit.edu.vn>
2023-05-18 19:53:32 +0700

    update

--- mini_mart_android/app/src/main/java/com/example/mini_mart_android/MainActivity.java
+++ mini_mart_android/app/src/main/java/com/example/mini_mart_android/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Toast;
 
 //import com.example.mini_mart_android.adapter.CategoryAdapter;
 //import com.example.mini_mart_android.adapter.ProductAdapter;
@@ -24,8 +25,7 @@ public class MainActivity extends AppCompatActivity {
     ActionBarDrawerToggle drawerToggle;
     ImageView setting;
     private RecyclerView discountedRecycler, categoryRecycler;
-    //private ProductAdapter productAdapter;
-    //private CategoryAdapter categoryAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,77 +37,24 @@ public class MainActivity extends AppCompatActivity {
 
         discountedRecycler = findViewById(R.id.discountedRecycler);
         categoryRecycler = findViewById(R.id.categoryRecycler);
-        //productAdapter= new ProductAdapter(this);
-        //categoryAdapter = new CategoryAdapter(this);
+
+        //btnKeyboard.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View view) {
+
+                //Intent intent = new Intent(historyActivity.this, MainActivity.class);
+                //startActivity(intent);
+
+                //Toast.makeText(historyActivity.this, "Go to the keyboard page", Toast.LENGTH_SHORT).show();
 
 
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
-        discountedRecycler.setLayoutManager(gridLayoutManager);
-        categoryRecycler.setLayoutManager(gridLayoutManager);
-
-        //productAdapter.setData(getListProduct());
-        //categoryAdapter.setData(getListCategory());
-        //discountedRecycler.setAdapter(productAdapter);
-        //categoryRecycler.setAdapter(categoryAdapter);
-
-        setting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent back = new Intent(MainActivity.this, Header.class);
-                startActivity(back);
-                finish();
-            }
-        });
-
-
-
+            //}
+        //});
     }
-    //    call from api
-    //private List<Product> getListProduct() {
-       // List<Product> list = new ArrayList<>();
-       // list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-       // list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-       // list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-       // list.add(new Product(R.drawable.img, "produc1"));
 
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
-        //list.add(new Product(R.drawable.img, "produc1"));
+}
 
-        //return list;
 
-    }
-    //private List<Category> getListCategory() {
-        //List<Category> list = new ArrayList<>();
-        //list.add(new Category( "category1"));
-        //list.add(new Category( "category1"));
-        //list.add(new Category( "category1"));
-        //list.add(new Category( "category1"));
-        //list.add(new Category( "category1"));
-        //list.add(new Category( "category1"));
-        //list.add(new Category( "category1"));
-        //list.add(new Category( "category1"));
-        //list.add(new Category( "category1"));
-        //list.add(new Category( "category1"));
-       // list.add(new Category( "category1"));
 
-        //return list;
 
-    //}
-//}
+
